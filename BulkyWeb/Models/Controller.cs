1d1ab1a91757cc0a614c1b652578874449f2fdae
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-05-24 16:29:49 +0200

    added Controller class in Model order

---
+++ BulkyWeb/Models/Controller.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Controller
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int  DisplayOrder { get; set; }
+    }
+}
